0b40df5c6d732e40c5f8655e196adf2d3a3617b5
zenott <mblevel@gmail.com>
2018-08-20 14:12:34 +0200

    Add previewer functionality

--- src/App.js
+++ src/App.js
@@ -2,7 +2,21 @@ import React, { Component } from 'react';
 import { MarkdownPreview } from 'react-marked-markdown';
 import './App.css';
 
+const placeholder='# Welcome to my React Markdown Previewer!\n\n## This is a sub-heading...\n### And here\'s some other cool stuff:\n  \nHeres some code, `<div></div>`, between 2 backticks.\n\n```\n// this is multi-line code:\n\nfunction anotherExample(firstLine, lastLine) {\n  if (firstLine == \'```\' && lastLine == \'```\') {\n    return multiLineCode;\n  }\n}\n```\n  \nYou can also make text **bold**... whoa!\nOr _italic_.\nOr... wait for it... **_both!_**\nAnd feel free to go crazy ~~crossing stuff out~~.\n\nThere\'s also [links](https://www.freecodecamp.com), and\n> Block Quotes!\n\nAnd if you want to get really crazy, even tables:\n\nWild Header | Crazy Header | Another Header?\n------------ | ------------- | ------------- \nYour content can | be here, and it | can be here....\nAnd here. | Okay. | I think we get it.\n\n- And of course there are lists.\n  - Some are bulleted.\n     - With different indentation levels.\n        - That look like this.\n\n\n1. And there are numbererd lists too.\n1. Use just 1s if you want! \n1. But the list goes on...\n- Even if you use dashes or asterisks.\n* And last but not least, let\'s not forget embedded images:\n\n![React Logo w/ Text](https://goo.gl/Umyytc)\n';
+
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      value: placeholder
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(event){
+    this.setState({
+      value: event.target.value
+    });
+  }
   render() {
     return (
       <div className="App">
@@ -11,7 +25,7 @@ class App extends Component {
             <span>Editor</span>
           </div>
           <div id="editor-content">
-            <textarea />
+            <textarea onChange={this.handleChange} value={this.state.value}/>
           </div>
         </div>
         <div id="previewer">
@@ -19,7 +33,7 @@ class App extends Component {
           <span>Previewer</span>
           </div>
           <div id="previewer-content">
-            <MarkdownPreview value={ "## valami" }/>
+            <MarkdownPreview value={ this.state.value }/>
           </div>
         </div>
       </div>
