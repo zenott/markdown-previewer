fbad2c25fd7228296376104cba9ab2eeefa25d90
zenott <mblevel@gmail.com>
2018-08-20 13:46:33 +0200

    Add layout

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div id="editor">
+          <div id="editor-titlebar" className="titlebar">
+            <span>Editor</span>
+          </div>
+          <div id="editor-content">
+            <textarea />
+          </div>
+        </div>
+        <div id="previewer">
+          <div id="previewer-titlebar" className="titlebar">
+          <span>Previewer</span>
+          </div>
+          <div id="previewer-content">
+          </div>
+        </div>
       </div>
     );
   }
